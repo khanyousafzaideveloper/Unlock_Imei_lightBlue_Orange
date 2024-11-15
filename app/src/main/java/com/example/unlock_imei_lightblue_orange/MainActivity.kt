d87a715e3bfe55d5a3fdafd27ff30dd5f51b9cdb
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2024-11-15 14:14:56 +0500

    first

---
+++ app/src/main/java/com/example/unlock_imei_lightblue_orange/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.unlock_imei_lightblue_orange
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unlock_imei_lightblue_orange.ui.theme.Unlock_imei_lightBlue_orangeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Unlock_imei_lightBlue_orangeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Unlock_imei_lightBlue_orangeTheme {
+        Greeting("Android")
+    }
+}
